f0bd19a3e2b1ebefdde055c5e3407501966e302c
Joe Fabisevich <Joe@fabisevi.ch>
2017-09-25 13:17:46 -0400

    Adding an example project

---
+++ Example/Example/CustomView.swift
@@ -0,0 +1,87 @@
+import UIKit
+
+// We're setting up a custom `UIView` instead of using a custom `UITableViewCell` subclass.
+final class CustomView: UIView {
+    
+    let actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        return button
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
+        
+        return label
+    }()
+
+    let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
+        
+        return label
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.spacing = 8.0
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.axis = .vertical
+
+        return stackView
+    }()
+    
+    // MARK: Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setup()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+private extension CustomView {
+    
+    func setup() {
+        self.addSubview(self.stackView)
+
+        self.stackView.addArrangedSubview(self.titleLabel)
+        self.stackView.addArrangedSubview(self.subtitleLabel)
+        self.stackView.addArrangedSubview(self.actionButton)
+
+        self.setupConstraints()
+    }
+    
+    func setupConstraints() {
+        self.stackView.translatesAutoresizingMaskIntoConstraints = false
+        self.stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        self.stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        self.stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        self.stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+}
+
+// This extension can live anywhere in the same target. I tend to have all my `ReusableGenericView`s
+// in a separate folder to not couple it directly to the view, but it's up to you!
+//
+// You can even leave the implementation empty, there is a default implementation.
+extension CustomView: ReusableGenericView {
+
+    func prepareForReuse() {
+        self.titleLabel.text = ""
+        self.subtitleLabel.text = ""
+        self.actionButton.setTitle(nil, for: .normal)
+    }
+    
+}
---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,106 @@
+import UIKit
+
+private struct RowContent {
+
+    let title: String
+    let subtitle: String
+    let buttonTitle: String
+    let buttonColor: UIColor
+}
+
+class ViewController: UIViewController {
+
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 76.0
+        tableView.register(GenericTableCell<CustomView>.self)
+        tableView.tableFooterView = UIView()
+
+        return tableView
+    }()
+
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setup()
+    }
+
+    private let rowContents: [RowContent] = [
+        RowContent(title: "Famous Dogs", subtitle: "Bo Obama, Spuds McKenzie, The Taco Bell Dog, and more!", buttonTitle: "Don't like dogs?", buttonColor: #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)),
+        RowContent(title: "Famous Cats", subtitle: "Grumpy Cat, Maru, Lil Bub, Nala, Tigger, and more!", buttonTitle: "Don't like cats?", buttonColor: #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)),
+        RowContent(title: "Famous Foxes", subtitle: "Fantastic Mr. Fox, Fox in Sox, That fox from the video \"What Does The Fox Say\", and more!", buttonTitle: "Don't like foxes?", buttonColor: #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)),
+        RowContent(title: "Famous Groundhogs", subtitle: "Punxsutawney Phil, State Island Chuck, Sir Walter Walley, and more!", buttonTitle: "Don't like groundhogs?", buttonColor: #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)),
+        RowContent(title: "Famous Lions", subtitle: "Mufasa, Mayor Lionheart, Scar, and more!", buttonTitle: "Don't like lions?", buttonColor: #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)),
+//        RowContent(title: <#T##String#>, subtitle: <#T##String#>, buttonTitle: <#T##String#>, buttonColor: <#T##UIColor#>),
+    ]
+
+}
+
+extension ViewController: UITableViewDelegate {
+ 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let currentRowContent = self.rowContents[indexPath.row]
+
+        let alertController = UIAlertController(title: "You want some \(currentRowContent.title)", message: "Let's see 'em!", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Maybe later", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
+
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.rowContents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(forIndexPath: indexPath) as GenericTableCell<CustomView>
+
+        // You can configure this however and wherever you want, but we're doing it here for simplicity.
+        let currentRowContent = self.rowContents[indexPath.row]
+        cell.customView.titleLabel.text = currentRowContent.title
+        cell.customView.subtitleLabel.text = currentRowContent.subtitle
+        cell.customView.actionButton.setTitle(currentRowContent.buttonTitle, for: .normal)
+        cell.customView.actionButton.setTitleColor(UIColor.white, for: .normal)
+        cell.customView.actionButton.backgroundColor = currentRowContent.buttonColor
+        cell.customView.actionButton.addTarget(self, action: #selector(showAngryWarning), for: .touchUpInside)
+        
+        return cell
+    }
+    
+}
+
+private extension ViewController {
+    
+    func setup() {
+        self.title = "Famous Animals"
+        
+        self.view.addSubview(self.tableView)
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        self.setupConstraints()
+    }
+    
+    func setupConstraints() {
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        self.tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        self.tableView.topAnchor.constraint(equalTo: self.topLayoutGuide.topAnchor).isActive = true
+        self.tableView.bottomAnchor.constraint(equalTo: self.bottomLayoutGuide.bottomAnchor).isActive = true
+    }
+
+    @objc func showAngryWarning() {
+        let alertController = UIAlertController(title: "And why not?", message: "Some people are just downright animals…", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Goodbye", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+}
+
